ac28cdefbc4a9554dadd77e0999f41fbd798b538
yhao <102126854@qq.com>
2019-09-12 18:21:42 +0800

    导入表格

---
+++ hivemedia-common/src/main/java/cn/hivemedia/common/utils/excel/SimpleExportParameter.java
@@ -0,0 +1,26 @@
+package cn.hivemedia.common.utils.excel;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author 杨浩
+ * @create 2019-09-12 18:15
+ **/
+@Data
+public class SimpleExportParameter {
+
+    private String fileName = "";
+    private String titleEn = "";
+    private String title = "";
+    private String[] fieldsId;
+    private String[] fieldsName;
+    private String[] widths;
+    private int startIndex = 2;
+
+    private List<Map<String, Object>> dataList;
+
+
+}
